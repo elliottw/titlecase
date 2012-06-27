2d91dc7e31d79914492add38a4819032657d9d05
Elliott Williams <_@elliottw.com>
2012-06-27 13:24:38 -0400

    fixed indentation

--- src/noir_bootstrap/views/welcome.clj
+++ src/noir_bootstrap/views/welcome.clj
@@ -32,79 +32,79 @@
 
 
 (defn cap-first-alpha [word]
-   (if (re-seq #"[a-zA-Z0-9]" (str (first word)))
-              (clojure.string/capitalize word)
-              (str (first word) (clojure.string/capitalize (second word)) (subs word 2))))
+  (if (re-seq #"[a-zA-Z0-9]" (str (first word)))
+    (clojure.string/capitalize word)
+    (str (first word) (clojure.string/capitalize (second word)) (subs word 2))))
 
-          (defn in-list? [word word-list]
-              "takes in a word and a list of words and returns true if word is in list"
-              (if (empty? word-list)
-                    false
-                    (if (= word (first word-list))
-                            true
-                            (in-list? word (rest word-list)))))
+(defn in-list? [word word-list]
+  "takes in a word and a list of words and returns true if word is in list"
+  (if (empty? word-list)
+    false
+    (if (= word (first word-list))
+      true
+      (in-list? word (rest word-list)))))
 
-                (defn camel? [word]
-                    (if (and (Character/isLetter (first word)) (re-seq #"[A-Z]" (subs word 1)))
-                          true
-                          false))
+(defn camel? [word]
+  (if (and (Character/isLetter (first word)) (re-seq #"[A-Z]" (subs word 1)))
+    true
+    false))
 
-                (defn split-n-cap [word-with-dashes]
-                    "capitalizes all the bits within a dashed word"
-                    (clojure.string/join "-"
-                                            (map 
-                                                    (fn [x] (clojure.string/capitalize x)) 
-                                                    (clojure.string/split word-with-dashes #"[-]"))))
+(defn split-n-cap [word-with-dashes]
+  "capitalizes all the bits within a dashed word"
+  (clojure.string/join "-"
+                       (map 
+                         (fn [x] (clojure.string/capitalize x)) 
+                         (clojure.string/split word-with-dashes #"[-]"))))
 
-                  (defn starts-w-container? [word]
-                      (if 
-                        (or 
-                          (= \' (first word)) 
-                          (= \" (first word)) 
-                          (= \_ (first word)) 
-                          (= \( (first word)))
-                        true
-                        false))
-
-
-                  (defn end-cap [word]
-                      "takes in a single word and returns proper capitalization for first or last word in title"
-                      (if (or (camel? word) (camel? (subs word 1)))
-                            word
-                            (if (starts-w-container? word)
-                                    (str (first word) (clojure.string/capitalize (second word)) (subs word 2))
-                                    (str (clojure.string/capitalize (first word)) (subs word 1)))))
+(defn starts-w-container? [word]
+  (if 
+    (or 
+      (= \' (first word)) 
+      (= \" (first word)) 
+      (= \_ (first word)) 
+      (= \( (first word)))
+    true
+    false))
 
 
-                  (defn  cond-cap [wordstring]
-                      "conditionally capitalizes words in the middle of a title"
-                      (let 
-                            [first-word (first wordstring)
-                                first-letter (first (first wordstring))
-                                second-letter (second first-word)]
-                            (if (not (empty? wordstring))
-                                      (cond 
-                                                  (in-list? first-word short-words)
-                                                  (do (println "short-word" first-word) (cons first-word (cond-cap (rest wordstring))))
-                                                (camel? first-word)         
-                                                  (do (println "camel" first-word) (cons first-word (cond-cap (rest wordstring))))
-                                                (in-list? "." (re-seq #"[a-z.]" first-word))
-                                                  (do (println "dot" first-word) (cons first-word (cond-cap (rest wordstring))))
-                                                (in-list? "-" (re-seq #"[a-z-]" first-word))
-                                                (do (println "dash" first-word) (cons (split-n-cap first-word) (cond-cap (rest wordstring))))
-                                                  (starts-w-container? first-word)
-                                                  (do
-                                                                (println "container" first-word " -> " (subs first-word 1) "camel? " (camel? (subs first-word 1)))
-                                                                (if (camel? (subs first-word 1))
-                                                                                (do (println "still camel" first-word) (cons first-word (cond-cap (rest wordstring))))
-                                                                                (cons 
-                                                                                                  (str 
-                                                                                                                      first-letter
-                                                                                                                    (clojure.string/capitalize second-letter)
-                                                                                                                      (subs first-word 2))
-                                                                                                  (cond-cap (rest wordstring)))))
-                                                  :else
-                                                  (do (println "normal" first-word) (cons (clojure.string/capitalize first-word) (cond-cap (rest wordstring))))))))
+(defn end-cap [word]
+  "takes in a single word and returns proper capitalization for first or last word in title"
+  (if (or (camel? word) (camel? (subs word 1)))
+    word
+    (if (starts-w-container? word)
+      (str (first word) (clojure.string/capitalize (second word)) (subs word 2))
+      (str (clojure.string/capitalize (first word)) (subs word 1)))))
+
+
+(defn  cond-cap [wordstring]
+  "conditionally capitalizes words in the middle of a title"
+  (let 
+    [first-word (first wordstring)
+     first-letter (first (first wordstring))
+     second-letter (second first-word)]
+    (if (not (empty? wordstring))
+      (cond 
+        (in-list? first-word short-words)
+        (do (println "short-word" first-word) (cons first-word (cond-cap (rest wordstring))))
+        (camel? first-word)         
+        (do (println "camel" first-word) (cons first-word (cond-cap (rest wordstring))))
+        (in-list? "." (re-seq #"[a-z.]" first-word))
+        (do (println "dot" first-word) (cons first-word (cond-cap (rest wordstring))))
+        (in-list? "-" (re-seq #"[a-z-]" first-word))
+        (do (println "dash" first-word) (cons (split-n-cap first-word) (cond-cap (rest wordstring))))
+        (starts-w-container? first-word)
+        (do
+          (println "container" first-word " -> " (subs first-word 1) "camel? " (camel? (subs first-word 1)))
+          (if (camel? (subs first-word 1))
+            (do (println "still camel" first-word) (cons first-word (cond-cap (rest wordstring))))
+            (cons 
+              (str 
+                first-letter
+                (clojure.string/capitalize second-letter)
+                (subs first-word 2))
+              (cond-cap (rest wordstring)))))
+        :else
+        (do (println "normal" first-word) (cons (clojure.string/capitalize first-word) (cond-cap (rest wordstring))))))))
 
 
 
@@ -173,5 +173,5 @@
       [:div.span3 [:font {:color "blue"} "For Step-by-Step Directions Email someone@gmail.com"]]]))
 
 (defpage "/assumptions" []
-         (common/layout
-           [:p "All user entered capitalization is correct (e.g. The Doors will not be corrected to the Doors)"]))
+  (common/layout
+    [:p "All user entered capitalization is correct (e.g. The Doors will not be corrected to the Doors)"]))
